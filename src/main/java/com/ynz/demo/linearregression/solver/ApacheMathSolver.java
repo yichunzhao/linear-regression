55043b825152a90c7dac6f606645e2675dfc8add
yichunzhao <zhaoyichun@hotmail.com>
2021-02-01 16:08:06 +0100

    rounding up double to two decimals

--- src/main/java/com/ynz/demo/linearregression/solver/ApacheMathSolver.java
+++ src/main/java/com/ynz/demo/linearregression/solver/ApacheMathSolver.java
@@ -6,22 +6,29 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.math3.stat.regression.SimpleRegression;
 import org.springframework.stereotype.Component;
 
+import java.text.DecimalFormat;
+
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class ApacheMathSolver implements RegressionSolver {
+    private final DecimalFormat df = new DecimalFormat("0.00");
     private final SimpleRegression simpleRegression;
 
     @Override
     public int isXYPositiveRelated(double[][] dataSet) {
+        //clean up solver
+        simpleRegression.clear();
+
         //add solver data
         simpleRegression.addData(dataSet);
 
         double slop = simpleRegression.getSlope();
         double intercept = simpleRegression.getIntercept();
         double r = simpleRegression.getR();
+        double rSquare = simpleRegression.getRSquare();
 
-        log.info("slop: " + slop + " intercept: " + intercept + " R: " + r);
+        log.info("slop: " + df.format(slop) + " intercept: " + df.format(intercept) + " R: " + df.format(r) + " R square: " + df.format(rSquare));
 
         return slop > 0 ? 1 : -1;
     }
